1986924b73e1998fd781e5cd91fa494b0cd072d3
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-09-26 12:52:24 +0100

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { completeTask } = require('../controllers/taskController');
+const { getTasks, completeTask } = require('../controllers/taskController');
 const { authenticateToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
+router.get('/', authenticateToken, getTasks);
 router.post('/:taskId/complete', authenticateToken, completeTask);
 
 module.exports = router;
