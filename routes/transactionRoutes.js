ad7b95db3e4cdecaecfac81670e6ea5203ccdc4b
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-09-26 12:51:48 +0100

    Update transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { deposit, withdraw } = require('../controllers/transactionController');
+const { deposit, withdraw, getTransactions } = require('../controllers/transactionController');
 const { authenticateToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/deposit', authenticateToken, deposit);
 router.post('/withdraw', authenticateToken, withdraw);
+router.get('/', authenticateToken, getTransactions);
 
 module.exports = router;
